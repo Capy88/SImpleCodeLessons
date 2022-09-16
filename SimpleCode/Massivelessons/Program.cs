1a6deb527b8490435717a8e41cb6e366ba0156fc
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-09-16 21:10:57 +0300

    massive

---
+++ SimpleCode/Massivelessons/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] qwe ={ 4,8,6,66,48};
+int result = 0;
+for (int i = 0; i < qwe.Length; i++)
+{
+    if (qwe[i] % 2 == 0) ;
+    {
+        result = result + qwe[i];
+    }
+}
+
+Console.WriteLine(result);
+Console.ReadLine();
+
+
+
+
+
