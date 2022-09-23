a49526184b4411697491ebf9b40e8106dd73f749
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-09-23 18:44:52 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿int GetNumber(int num)
+{
+    Console.WriteLine($"Введите {num} число");
+    string inputNumber1 = Console.ReadLine();
+    int number1;
+    bool isSuccess = int.TryParse(inputNumber1, out number1);
+    while (isSuccess == false)
+    {
+        Console.WriteLine("Введите цифру");
+        inputNumber1 = Console.ReadLine();
+
+        isSuccess = int.TryParse(inputNumber1, out number1);
+    }
+
+    return number1;
+}
+
+
+int num1 = GetNumber(1); 
+
+int num2 = GetNumber(2);
+
+int num3 = GetNumber(3);    
+if (num1 > num3)
+{
+    Console.WriteLine($"число {num1} больше числа {num3}");
+}
+else if (num1 == num3)
+{
+    Console.WriteLine($"число {num1} равно числу {num3}");
+}
+else
+{
+    Console.WriteLine($"число {num3} больше числа {num1}");
+}
+
