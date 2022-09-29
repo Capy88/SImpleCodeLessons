2d76f851ee2bb114076ca23ea77595714f9c91fa
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-09-29 22:31:16 +0300

    123

---
+++ TaskStudents/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Ведите вес Максима");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите вес Данила");
+int den = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите вес Ивана");
+int ivan = int.Parse(Console.ReadLine());
+if (max  > ivan * 2  )
+{
+    Console.WriteLine("Правда");
+}
+else if (den * 2 > ivan * 2)
+{
+    Console.WriteLine("Правда");
+}
+else if (ivan * 2 > max * 2)
+{
+    Console.WriteLine("Правда");
+}
+
+else if (ivan * 2 > den * 2)
+{
+    Console.WriteLine("Правда");
+}
+
+else if (max * 2 > den * 2)
+{
+    Console.WriteLine("Правда");
+}
+
+else if (den * 2 > max * 2)
+{
+    Console.WriteLine("Правда");
+}
+else
+{
+    Console.WriteLine("Ложь");
+}
