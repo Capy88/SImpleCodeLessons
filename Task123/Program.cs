ed4ce05c5bf13304b119ef9e1899d6f658c0ad01
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-09-30 21:16:56 +0300

    243

---
+++ Task123/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] a = { 60, 130, 74, 23, 4 };
+int minvalue = a[0];
+int maxvalue = a[0];
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] < minvalue)
+    {
+        minvalue = a[i];
+    }
+    if (a[i] > maxvalue)
+    {
+        maxvalue = a[i];
+    }
+}
+Console.WriteLine(minvalue);
+Console.WriteLine(maxvalue);
