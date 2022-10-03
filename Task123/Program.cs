8bf06dc0a03c11e7d086443c7524cbd0a1f3a371
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-10-03 23:24:23 +0300

    yep

--- Task123/Program.cs
+++ Task123/Program.cs
@@ -1,25 +1,39 @@
-﻿int[] weight = { 65, 140,74 };
+﻿Console.WriteLine($"введите кол-во студентов");
+string students = Console.ReadLine();
+int weightStudents = int.Parse(students);
 
-Console.WriteLine($"вес Максима = {weight[0]}");
-Console.WriteLine($"вес Ильи = {weight[1]}");
-Console.WriteLine($"вес Егора = {weight[2]}");
+int[] weight = new int [weightStudents];
 
-int minValue = weight[0];
-int maxValue = weight[0];
+for (int i = 0;i < weightStudents ; i++)
+{
+    Console.WriteLine($"Введите вес {i} студента, введите 999 для выхода");
+    string b = Console.ReadLine();
+    int a = int.Parse(b);
+    weight [i] = a;
+    if (a == 999)
+    {
+        Console.WriteLine("end");
+        break;
+    }
+}
+
+int minWeight = weight[0];
+int maxWeight = weight[0];
 
 for (int i = 0; i < weight.Length; i++)
 {
-    if (weight[i] < minValue)
+
+    if (weight[i] < minWeight)
     {
-        minValue = weight[i];
+        minWeight = weight[i];
     }
-    if (weight[i] > maxValue)
+    if (weight[i] > maxWeight)
     {
-        maxValue = weight[i];
+        maxWeight = weight[i];
     }
 }
- 
-if (minValue * 2 < maxValue )   
+
+if (minWeight * 2 < maxWeight)
 {
     Console.WriteLine("правда");
 }
