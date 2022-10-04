7669d0e7846d006898e2a05ba02d07b33fdb2f47
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-10-04 23:14:33 +0300

    12345

--- Task123/Program.cs
+++ Task123/Program.cs
@@ -1,26 +1,44 @@
-﻿Console.WriteLine($"введите кол-во студентов");
-string students = Console.ReadLine();
-int weightStudents = int.Parse(students);
+﻿using System.Diagnostics;
 
-int[] weight = new int [weightStudents];
+//Console.WriteLine($"введите кол-во студентов");
+//string students = Console.ReadLine();
 
-for (int i = 0;i < weightStudents ; i++)
+//int weightStudents;
+//bool result = int.TryParse(students, out weightStudents);
+//if (result == false)
+//{
+//    Console.WriteLine("некорректное значение");
+//    Process.GetCurrentProcess().Kill();
+//}
+
+//int[] weight = new int[weightStudents];
+List <int> weight = new List<int>();
+
+for (int i = 0; ; i++)
 {
-    Console.WriteLine($"Введите вес {i} студента, введите 999 для выхода");
+    Console.WriteLine($"Введите вес {i} студента,затем введите любую букву для выхода");
+    int WeightOfStudent;
     string b = Console.ReadLine();
-    int a = int.Parse(b);
-    weight [i] = a;
-    if (a == 999)
+    bool Result = int.TryParse(b, out WeightOfStudent);
+    //weight[i] = WeightOfStudent;
+    weight.Add(WeightOfStudent);
+    if (Result == false)
     {
-        Console.WriteLine("end");
+        Console.WriteLine("программа завершена");
         break;
     }
+
+    //else if (a == int.Parse("q") )
+    //{
+    //    Console.WriteLine("программа завершена");
+    //    Process.GetCurrentProcess().Kill();
+    //}
 }
 
 int minWeight = weight[0];
 int maxWeight = weight[0];
 
-for (int i = 0; i < weight.Length; i++)
+for (int i = 0; i < weight.Count; i++)
 {
 
     if (weight[i] < minWeight)
