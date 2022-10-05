551b84d75bbcd82666e76d248c1e91b9fdad5401
nikolay filatov <NVFilatov@alfabank.ru>
2022-10-05 22:46:24 +0300

    Доделал задачу

--- Task123/Program.cs
+++ Task123/Program.cs
@@ -1,57 +1,73 @@
-﻿using System.Diagnostics;
+﻿
 
-List<int> weight = new List<int>();
-string exit = "q";
-bool inputedExit;
-for (int i = 1; ; i++)
+List<int> studentsWeight = new List<int>();
+bool inputedExit = false; //переменная означающая, что пользователь захотел завершить программу при вводе данных
+
+while(true)
 {
-    Console.WriteLine($"Введите вес {i} студента,затем введите любую букву для выхода либо введите q для завершения программы");
-    int WeightOfStudent;
+    Console.WriteLine($"Введите вес {studentsWeight.Count + 1} студента или введите 'q' для завершения программы или любую другую букву для окончания ввода информации о студентах");
     string inputstring = Console.ReadLine();
-    bool Result = int.TryParse(inputstring, out WeightOfStudent);
-    weight.Add(WeightOfStudent);
-   
 
-    if (Result == false)
+    if(inputstring == "q")
+    {
+        inputedExit = true; //Эта переменная потом проверяется ниже и если она true, то программа больше ничего не делает
+        break;
+    }
+
+    int weightOfStudent;
+    bool inputIsNumber = int.TryParse(inputstring, out weightOfStudent); // Проверяем, что ввел число, а не другой символ
+
+    if(inputIsNumber == false) //и выходим из ввода весов если ввел не числа
     {
         break;
     }
-    else if (inputstring == exit);
-    {
-        Console.WriteLine("программа завершена");
-        Process.GetCurrentProcess().Kill();
-    }
+
+    studentsWeight.Add(weightOfStudent);
 }
 
-int minWeight = weight[0];
-int maxWeight = weight[0];
-
-for (int i = 0; i < weight.Count; i++)
+if (inputedExit)
 {
-
-    if (weight[i] < minWeight)
-    {
-        minWeight = weight[i];
-    }
-    if (weight[i] > maxWeight)
-    {
-        maxWeight = weight[i];
-    }
-}
-
-if (minWeight * 2 < maxWeight)
-{
-    Console.WriteLine("правда");
+    Console.WriteLine("Вы завершили работу с прогрммой. Программа будет закрыта после нажатия на любую клавишу");
 }
 else
 {
-    Console.WriteLine("ложь");
+    if (studentsWeight.Count < 2)
+    {
+        Console.WriteLine("Нужно ввести хотя бы 2х студентов, чтобы ответить на вопрос задачи");
+    }
+    else
+    {
+        CompareMinAndMax(studentsWeight); //Вынес код в отдельную функцию, чтобы было более читабельно и понятно читать код
+        //Чтоб код умещался на одном экране так сказаить, а код функции уже можно было смотреть отдельно
+        //И название функции уже говорит о том, что делает функция и внутрь нее даже можно не заглядывать, чтобы понять что она делает
+    }
 }
 
+//Эта функция выполняет одно конкретное действие - из принимаемого снаружи массива чисел находит максимальное и минимальное и сравнивает их
+void CompareMinAndMax(List<int> studentsWeight)
+{
+    int minWeight = studentsWeight[0];
+    int maxWeight = studentsWeight[0];
 
+    for (int i = 0; i < studentsWeight.Count; i++)
+    {
 
+        if (studentsWeight[i] < minWeight)
+        {
+            minWeight = studentsWeight[i];
+        }
+        if (studentsWeight[i] > maxWeight)
+        {
+            maxWeight = studentsWeight[i];
+        }
+    }
 
-
-
-
-
+    if (minWeight * 2 < maxWeight)
+    {
+        Console.WriteLine("Вес самого тяжелого больше чем в 2 раза больше наименьшего веса");
+    }
+    else
+    {
+        Console.WriteLine("Вес самого тяжелого не в 2 раза больше наименьшего веса");
+    }
+}
