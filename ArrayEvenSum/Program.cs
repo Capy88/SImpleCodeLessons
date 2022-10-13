abcd15db3ca7028995b651e9f5a22750b1ea967f
maksimcik228@gmail.com <maksimcik228@gmail.com>
2022-10-13 17:51:34 +0300

    array sum

---
+++ ArrayEvenSum/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new[] { 2, 6, 8, 9, 12, 66 };
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+	if (array[i] % 2 == 0)
+	{
+		result = result + array[i];
+	}
+}
+
+Console.WriteLine(result);
