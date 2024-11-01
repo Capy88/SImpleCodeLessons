f7ba376d228aefda1a1612d3208212046c24b678
Max <Max@CAPY-COMP>
2024-11-01 17:07:26 +0300

    calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+string str = Console.ReadLine();
+int x = int.Parse(str);
+
+Console.WriteLine("Введите второе число");
+str = Console.ReadLine();
+int y = int.Parse(str);
+
+int result = x + y;
+Console.Write("Сумма двух чисел: ");
+Console.WriteLine(result);
+Console.WriteLine($"Разность двух чисел: {x - y}");
+Console.WriteLine($"Произведение двух чисел: {x * y}");
+Console.WriteLine($"Отношение двух чисел: {x / y}");
